520d2c330c6bfe1cad2bc9b69580f75bfba7ee20
Scott Weinstein <sweinstein27@gmail.com>
2018-08-01 13:53:20 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,13 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  def initialize (name, weight, height)
+    @name=name
+    @weight=weight
+    @height=height
+  end
+
+  def self.all
+    PIRATE
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  def initialize(name, type, booty)
+    @name=name
+    @type=type
+    @booty=booty
+  end
+
+  def self.all
+    SHIP
+  end
+
+  def self.clear
+    SHIP.clear
+  end
 end
